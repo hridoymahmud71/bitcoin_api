2cae31d9a33baa3033163a853335ea69587fe891
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-02 21:00:13 +0600

    hmm

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get(
-    'getBitcoinInfo',
+    '/getBitcoinInfo',
     [BitcoinController::class, 'getBitcoinInfo']
 )->name('getBitcoinInfo');
 
